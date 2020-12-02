925c1df4c72e4f9eb19a5a3b170a45b5f23c3694
luongdangvp2000 <luongdangvp2000@gmail.com>
2020-12-02 11:10:28 +0700

    delete navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
 
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light" fixed="top">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light" data-spy="affix" data-offset-top="197">
       <div className="col-9 logo" xs="auto" md="auto">
         <a className="navbar-brand" href="#">House For U <i className="fas fa-home"></i></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,7 +20,7 @@ function Navbar() {
         </button>
       </div>
 
-      <div className="col-3" xs="auto" md="auto" className="justify-content-end">
+      <div className="container-fluid col-3" xs="auto" md="auto" className="justify-content-end">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
