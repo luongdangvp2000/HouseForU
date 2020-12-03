3f59e51608555d5dcde87d305ceb960069ba85d6
luongdangvp2000 <luongdangvp2000@gmail.com>
2020-12-04 01:46:45 +0700

    add image to house template

--- src/components/HouseTemplate.js
+++ src/components/HouseTemplate.js
@@ -1,10 +1,19 @@
 import './HouseTemplate.css'
 import React, { Component } from 'react'
+import Image from 'react-bootstrap/Image'
+import apartment from '../images/apartment.jpg'
+
+console.log(apartment);
 
 
 function HouseTemplate() {
-    return(
-        <h1>House Template</h1>
+    return (
+        <>
+            <h1>House Template</h1>
+            <div>
+                <Image src={apartment} thumbnail/>
+            </div>
+        </>
     );
 }
 
